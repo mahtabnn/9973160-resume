a68f8b0d760d00cc481771ab5cb424b006dbd08d
mahtabnn <mahtab.nicknezhad@gmail.com>
2026-01-23 18:28:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,52 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from schemas import ProjectRequest
+from fastapi import FastAPI
+from schemas import ProjectRequest, WebSocketDisconnect
+from fastapi import WebSocket
 
 app = FastAPI()
-
-# ذخیره موقت درخواست‌ها
-requests_list = []
-
-# شمارش کاربران آنلاین
-online_users = 0
-
 
 @app.get("/")
 def home():
     return {"message": "Server is running"}
 
 
-# ثبت درخواست پروژه
+#ثبت درخواست
+
+requests_list = []
+
 @app.post("/request")
 def create_request(data: ProjectRequest):
     requests_list.append(data)
     return {"status": "saved"}
 
 
-# دیدن همه درخواست‌ها (ادمین ساده)
+@app.post("/login")
+def login(username: str, password: str):
+    if username == "admin" and password == "1234":
+        return {"token": "fake-jwt-token"}
+    return {"error": "unauthorized"}
+
 @app.get("/admin/requests")
-def get_requests():
+def get_requests(token: str):
+    if token != "fake-jwt-token":
+        return {"error": "forbidden"}
     return requests_list
 
 
-# WebSocket برای کاربران آنلاین
+
+
+
+#تعداد کاربر انلاین
+online_users = 0
+
 @app.websocket("/ws")
 async def websocket_endpoint(ws: WebSocket):
     global online_users
     await ws.accept()
     online_users += 1
     await ws.send_text(str(online_users))
-
     try:
         while True:
             await ws.receive_text()
-    except WebSocketDisconnect:
+    except:
         online_users -= 1
+
